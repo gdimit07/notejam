185e91b44306a2ee1325e746b2bd75a27e53b2da
gdimit07 <giorgos_11@hotmail.com>
2021-10-31 23:38:37 +0200

    terraform node and firewall rule IaC

---
+++ infra/firewall/main.tf
@@ -0,0 +1,17 @@
+provider "google" {
+  project = "cks-328210"
+  region  = "europe-west3"
+  zone    = "europe-west3-c"
+}
+
+
+resource "google_compute_firewall" "rules" {
+  project     = "cks-328210"
+  name        = "k8s-nodeports"
+  network     = "default"
+
+  allow {
+    protocol  = "tcp"
+    ports     = ["30000-40000"]
+  }
+}
---
+++ infra/node/main.tf
@@ -0,0 +1,23 @@
+provider "google" {
+  project = "cks-328210"
+  region  = "europe-west3"
+  zone    = "europe-west3-c"
+}
+
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "k8s-node"
+  machine_type = "e2-medium"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1804-bionic-v20201014"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
